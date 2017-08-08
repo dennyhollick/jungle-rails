defeca3786a1ec46b0467de0d0a4aa0fdbc66f17
Denny Hollick <dennyhollick@gmail.com>
2017-08-07 20:26:54 -0700

    Created user auth method and added before_save to lowercase emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  before_save { |user| user.email = email.downcase }
 
-
+  def self.authenticate_with_credentials(email, password)
+    clean_email = email.gsub(/\s+/, "").downcase 
+    user = User.find_by_email(clean_email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end  
+  end
 end
