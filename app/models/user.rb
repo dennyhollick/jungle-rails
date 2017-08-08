6a921eafef70aac16cff75072a66acccf80c0c68
Denny Hollick <dennyhollick@gmail.com>
2017-08-07 19:12:29 -0700

    Added vaidations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
 end
