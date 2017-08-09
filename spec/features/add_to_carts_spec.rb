4c6a2a72dedcc5d01f98c2206c0e530ddd08d365
Denny Hollick <dennyhollick@gmail.com>
2017-08-08 18:55:06 -0700

    Added test to check add to cart function

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds something to their cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see the details of the product" do
+    # ACT
+    visit root_path
+    first('article.product').first('a.btn.btn-primary').click
+
+    expect(page).to have_css("ul.nav.navbar-nav.navbar-right", text: " My Cart (1)")
+
+    # save_screenshot
+  end
+end
